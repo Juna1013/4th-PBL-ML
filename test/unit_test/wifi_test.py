d558ecfc743984b51b9f08e0aa4adaf8b87ae3f3
Juna1013 <jmeron.530@gmail.com>
2025-11-14 10:13:36 +0900

    wifi接続テストの追加

--- test/unit_test/wifi_test.py
+++ test/unit_test/wifi_test.py
@@ -1,8 +1,11 @@
 import network
 import time
+import urequests
+
 SSID = 'Juna1013'
 PW = 'f94s7uu4'
 
+# WiFi接続
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect(SSID, PW)
@@ -11,9 +14,20 @@ while wlan.isconnected() == False:
     print('Connecting to Wi-Fi router')
     time.sleep(1)
 
-wlan_status = wlan.ifconfig()
-print('Connected')
-print(f'IP Adress: {wlan_status[0]}')
-print(f'Netmask: {wlan_status[1]}')
-print(f'Default Gateway: {wlan_status[2]}')
-print(f'Name Server: {wlan_status[3]}')
+ip_info = wlan.ifconfig()
+print("Connected!")
+print("IP Adress:", ip_info[0])
+PORT = 8000
+
+# FastAPIサーバーにアクセス
+SERVER_IP = "10.73.246.50"
+PORT = 8000
+
+try:
+    url = f"http://{SERVER_IP}:{PORT}/ping"
+    print("Requesting:", url)
+    response = urequests.get(url)
+    print("Response:", response.text)
+    response.close()
+except Exceprion as e:
+    print("Error:", e)
