6185d276f8c9e4dc45fc2fa9e371c2cbe81d4c00
Juna1013 <jmeron.530@gmail.com>
2025-11-21 09:01:06 +0900

    モーターの回転方向の修正

--- test/integration_test/main.py
+++ test/integration_test/main.py
@@ -18,8 +18,8 @@ print("BASE_PWM:", BASE_PWM)
 # --- モーターの準備 ---
 left_fwd = PWM(Pin(LEFT_REV_PIN))
 left_rev = PWM(Pin(LEFT_FWD_PIN))
-right_fwd = PWM(Pin(RIGHT_REV_PIN))
-right_rev = PWM(Pin(RIGHT_FWD_PIN))
+right_fwd = PWM(Pin(RIGHT_FWD_PIN))
+right_rev = PWM(Pin(RIGHT_REV_PIN))
 
 # 周波数を1000Hzに設定
 left_fwd.freq(1000)
